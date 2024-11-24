58c71947255349932d2082592f824ee0c0b2ed15
gucci67 <>
2024-11-24 23:04:48 +0900

    lambda layer で配置

--- test-app/lib/test-app-stack.ts
+++ test-app/lib/test-app-stack.ts
@@ -3,6 +3,8 @@ import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as nodejs from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import path = require("path");
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestAppStack extends cdk.Stack {
@@ -19,10 +21,19 @@ export class TestAppStack extends cdk.Stack {
             ],
         });
 
+        const layer = new lambda.LayerVersion(this, "LambdaLayer", {
+            code: lambda.Code.fromAsset(
+                path.join(__dirname, "../lambda/layer")
+            ), // レイヤーのディレクトリ
+            compatibleRuntimes: [lambda.Runtime.NODEJS_22_X], // 使用可能なランタイム
+            description: "A layer that includes a template.xlsx file",
+        });
+
         new nodejs.NodejsFunction(this, "Lambda", {
             functionName: "test-lambda",
             runtime: lambda.Runtime.NODEJS_22_X,
             entry: "lambda/src/index.ts",
+            layers: [layer],
             timeout: cdk.Duration.seconds(3),
             role,
             bundling: {
